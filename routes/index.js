c72dca1b533f4d0743b66051a8215197bb6b99fd
Henry Ou <henryouly@gmail.com>
2017-05-28 21:28:56 -0700

    add missing new files

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const secret = require('../secret');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  var Twitter = require('twitter');
+  var client = new Twitter({
+    consumer_key: secret.consumer_key,
+    consumer_secret: secret.consumer_secret,
+    access_token_key: secret.access_token_key,
+    access_token_secret: secret.access_token_secret
+  });
+  var params = {q: 'gboard'};
+  client.get('search/tweets', params, function(error, tweets, response) {
+    if (!error) {
+      res.render('index', {
+        title: 'Tweets - Gboard',
+        tweets: tweets
+      });
+    } else {
+      res.status(403).send(error).end();
+    }
+  });
+});
+
+module.exports = router;
