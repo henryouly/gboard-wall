8ce0ac2783d05f37c2b455641f04f469b291719c
Henry Ou <henryouly@gmail.com>
2017-05-29 14:34:47 -0700

    start to reload

---
+++ static/init.js
@@ -0,0 +1,14 @@
+const timeout = 10000;
+var randomTweet = function() {
+  console.log("randomTweet");
+  var images = $(".bg_photos img");
+  var index = Math.floor(Math.random() * (images.length + 1));
+  $("div.profile img").attr('src', images.eq(index).attr('src'));
+  $("div.profile_info p").text($(".username p").eq(index).text());
+  $("div.comment p").text($(".text p").eq(index).text());
+  setTimeout(randomTweet, timeout);
+}
+
+$(document).ready(function() {
+  setTimeout(randomTweet, timeout);
+});
